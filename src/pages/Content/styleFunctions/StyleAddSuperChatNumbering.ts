118e739851eb4749b4dd5441244ae99b376fd287
TsubasaHiga <info@higatsubasa.com>
2022-09-04 21:16:02 +0900

    update:#24 スパチャナンバリングの対象要素に'yt-live-chat-ticker-paid-sticker-item-renderer'を追加

--- src/pages/Content/styleFunctions/StyleAddSuperChatNumbering.ts
+++ src/pages/Content/styleFunctions/StyleAddSuperChatNumbering.ts
@@ -4,17 +4,20 @@ yt-live-chat-ticker-renderer {
   counter-reset: number 0;
 }
 
+yt-live-chat-ticker-paid-sticker-item-renderer,
 yt-live-chat-ticker-paid-message-item-renderer,
 yt-live-chat-ticker-sponsor-item-renderer,
 yt-live-chat-ticker-paid-sticker-item-renderer {
   width: auto !important;
 }
 
+yt-live-chat-ticker-paid-sticker-item-renderer #content,
 yt-live-chat-ticker-paid-message-item-renderer #content {
   display: flex;
   align-items: center;
 }
 
+yt-live-chat-ticker-paid-sticker-item-renderer #content:before,
 yt-live-chat-ticker-paid-message-item-renderer #content:before {
   border-right: 1px solid rgb(0 0 0 / 20%);
   box-sizing: border-box;
