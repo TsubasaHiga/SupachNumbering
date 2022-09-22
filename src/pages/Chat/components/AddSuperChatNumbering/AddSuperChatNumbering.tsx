251ed02d67c7d0472fcfdfc8c8778d750996d317
TsubasaHiga <info@higatsubasa.com>
2022-09-22 20:21:29 +0900

    update:#58 AddSuperChatNumberingの処理の一部を別moduleとして切り離し

--- src/pages/Chat/components/AddSuperChatNumbering/AddSuperChatNumbering.tsx
+++ src/pages/Chat/components/AddSuperChatNumbering/AddSuperChatNumbering.tsx
@@ -1,44 +1,10 @@
 import { memo, useCallback, useEffect, useState } from 'react'
 
 import useMutationObserver from '~/hooks/useMutationObserver'
-import GetSha256 from '~/modules/GetSha256'
-import GetUserNameFromTickerItemElement from '~/modules/GetUserNameFromTickerItemElement'
 import { NumberingType } from '~/types/SettingsType'
 
-/**
- * addUniqueNumberingString
- * @param element
- * @param numberingType
- * @param stringLength
- * @returns
- */
-const addUniqueNumberingString = async (
-  element: HTMLElement,
-  numberingType: NumberingType,
-  stringLength: number
-) => {
-  if (!element.id || element.id.length <= 0) return
-  // console.log('addUniqueNumberingString')
-
-  // スーパーチャットの要素を取得
-  const tickerPaidMessageItem = element.querySelector('#content') as HTMLElement
-  if (!tickerPaidMessageItem) return
-
-  // 文字列取得
-  let stringValue = ''
-
-  if (numberingType === 'uniqueId') {
-    stringValue = await GetSha256(element.id)
-  }
-
-  if (numberingType === 'uniqueUserName') {
-    stringValue = await GetUserNameFromTickerItemElement(element, 1000, 100)
-  }
-
-  // データ属性にユニークな文字列をセット
-  tickerPaidMessageItem.dataset.uniqueString =
-    stringLength > 0 ? stringValue.slice(0, stringLength) : stringValue
-}
+import AddUniqueNumberingString from './modules/AddUniqueNumberingString'
+import GetItemsElement from './modules/GetItemsElement'
 
 type Props = {
   stringLength: number
@@ -65,40 +31,10 @@ const AddSuperChatNumbering = ({
   // rootMutationCallback
   const rootMutationCallback = useCallback((mutations: MutationRecord[]) => {
     mutations.forEach((mutation: MutationRecord) => {
-      // childList以外の変更の時は処理しない
-      if (mutation.type !== 'childList') return
-
-      // addedNodesがない時は処理しない
-      if (!mutation.addedNodes || mutation.addedNodes.length <= 0) return
-
-      // addedNodesの0番目を取得
-      const addedNode = mutation.addedNodes[0] as HTMLElement | null | undefined
-
-      // 存在しない時は処理しない
-      if (!addedNode) return
-
-      // nodeTypeが1以外の時は処理しない
-      if (addedNode.nodeType !== 1) return
-
-      // 追加された要素を取得
-      const element = addedNode as HTMLElement | undefined
-
-      // elementが空の時は処理しない
-      if (!element) return
-
-      // elementのtagNameが'YT-LIVE-CHAT-TICKER-RENDERER'以外の時は処理しない
-      if (element.tagName !== 'YT-LIVE-CHAT-TICKER-RENDERER') return
-
-      // #itemsを取得
-      const elementItems = element.querySelectorAll(
-        '#items'
-      ) as NodeListOf<HTMLElement>
-
-      // elementItemsが存在しない時は処理しない
-      if (!elementItems || elementItems.length <= 0) return
+      const elementItems = GetItemsElement(mutation)
+      if (!elementItems) return
 
       // setElementItems
-      console.log('setElementItems')
       setElementItems(elementItems)
     })
   }, [])
@@ -119,8 +55,8 @@ const AddSuperChatNumbering = ({
         // 要素が存在しない時は処理しない
         if (!mutation.addedNodes[0]) return
 
-        // addUniqueNumberingString
-        addUniqueNumberingString(
+        // AddUniqueNumberingString
+        AddUniqueNumberingString(
           mutation.addedNodes[0] as HTMLElement,
           numberingType,
           stringLength
@@ -138,7 +74,7 @@ const AddSuperChatNumbering = ({
     if (!elementItems) return
     elementItems.forEach((target) => {
       target.childNodes.forEach((child) =>
-        addUniqueNumberingString(
+        AddUniqueNumberingString(
           child as HTMLElement,
           numberingType,
           stringLength
