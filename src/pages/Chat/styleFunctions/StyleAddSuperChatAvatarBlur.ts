0caa67ee43a5af10b4388eff826495494d80066d
TsubasaHiga <info@higatsubasa.com>
2023-01-15 06:10:58 +0900

    update:#66 アバター画像のモザイク適用範囲を#author-photo配下のimgのみに変更

--- src/pages/Chat/styleFunctions/StyleAddSuperChatAvatarBlur.ts
+++ src/pages/Chat/styleFunctions/StyleAddSuperChatAvatarBlur.ts
@@ -1,8 +1,8 @@
 const StyleAddSuperChatAvatarBlur = () => {
   const style = `
-yt-live-chat-ticker-renderer yt-img-shadow img,
-yt-live-chat-paid-message-renderer yt-img-shadow img,
-yt-live-chat-paid-sticker-renderer yt-img-shadow img {
+yt-live-chat-ticker-renderer yt-img-shadow#author-photo img,
+yt-live-chat-paid-message-renderer yt-img-shadow#author-photo img,
+yt-live-chat-paid-sticker-renderer yt-img-shadow#author-photo img {
   filter: blur(4px);
 }
 `
