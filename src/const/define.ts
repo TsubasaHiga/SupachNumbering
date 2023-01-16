d729e7c6f9d89e4321d9748b5c68839cd8449933
TsubasaHiga <info@higatsubasa.com>
2023-01-16 23:27:02 +0900

    update:#58 設定画面のheight値を変更

--- src/const/define.ts
+++ src/const/define.ts
@@ -2,7 +2,7 @@ const define = {
   STORE_URL: `https://chrome.google.com/webstore/detail/${chrome.runtime.id}`,
 
   POPUP_WIDTH: 660,
-  POPUP_HEIGHT: 600,
+  POPUP_HEIGHT: 800,
 
   ELEMENT_READY_OPTIONS: {
     timeout: 10000,
