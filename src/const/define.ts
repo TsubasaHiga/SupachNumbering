e796d91142d5ab1de767bdeac01d905446138160
TsubasaHiga <info@higatsubasa.com>
2023-01-17 02:37:07 +0900

    update:#58 設定画面のスクロールバーのデザインを変更

--- src/const/define.ts
+++ src/const/define.ts
@@ -1,7 +1,7 @@
 const define = {
   STORE_URL: `https://chrome.google.com/webstore/detail/${chrome.runtime.id}`,
 
-  POPUP_WIDTH: 660,
+  POPUP_WIDTH: 660 + 10, // スクロールバー分の10pxを追加（10pxは'src/pages/Popup/index.scss'に記載）
   POPUP_HEIGHT: 800,
 
   ELEMENT_READY_OPTIONS: {
