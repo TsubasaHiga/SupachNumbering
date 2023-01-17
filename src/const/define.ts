90cc4fc22dbf8a3dac3e660e3b9b273c535c04a7
TsubasaHiga <info@higatsubasa.com>
2023-01-17 23:29:34 +0900

    update:#51 設定画面のX軸スクロールバーが表示されないよう調整

--- src/const/define.ts
+++ src/const/define.ts
@@ -1,7 +1,7 @@
 const define = {
   STORE_URL: `https://chrome.google.com/webstore/detail/${chrome.runtime.id}`,
 
-  POPUP_WIDTH: 660 + 10, // スクロールバー分の10pxを追加（10pxは'src/pages/Popup/index.scss'に記載）
+  POPUP_WIDTH: 660 + 10 + 16, // スクロールバー分の10px + 16pxを追加（10pxは'src/pages/Popup/index.scss'に記載、16pxはWindowsのスクロールバー幅分）
   POPUP_HEIGHT: 800,
 
   ELEMENT_READY_OPTIONS: {
