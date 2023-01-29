f9aa4d786d28c73fd42eca2b05223162b54927d7
TsubasaHiga <info@higatsubasa.com>
2023-01-29 23:46:45 +0900

    update:#84 チャット欄の設定メニューの表示ON・OFF設定追加

--- src/pages/Chat/components/SettingsMenu/SettingsMenu.tsx
+++ src/pages/Chat/components/SettingsMenu/SettingsMenu.tsx
@@ -5,6 +5,8 @@ import MenuList from '@mui/material/MenuList'
 import { memo, useCallback, useState } from 'react'
 
 import Logo from '~/assets/img/icon-34.png'
+import { useSettingsStore } from '~/store/atoms/useSettingsStore'
+import { SettingsType } from '~/types/SettingsType'
 
 import MenuFooter from './MenuFooter/MenuFooter'
 import MenuHeader from './MenuHeader/MenuHeader'
@@ -17,6 +19,9 @@ const ITEM_HEIGHT = 48
 const SettingsMenu = (): JSX.Element => {
   const manifest = chrome.runtime.getManifest()
 
+  const [settings] = useSettingsStore()
+  const { isDisplayChatOnSettingMenu }: SettingsType = settings
+
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
   const open = Boolean(anchorEl)
   const handleClick = useCallback((event: React.MouseEvent<HTMLElement>) => {
@@ -27,44 +32,48 @@ const SettingsMenu = (): JSX.Element => {
   }, [])
 
   return (
-    <div className={styles.root}>
-      <IconButton
-        className={styles.button}
-        aria-label="more"
-        id="long-button"
-        aria-controls={open ? 'long-menu' : undefined}
-        aria-expanded={open ? 'true' : undefined}
-        aria-haspopup="true"
-        onClick={handleClick}
-      >
-        <img src={chrome.runtime.getURL(Logo)} alt={manifest.name} />
-      </IconButton>
+    <>
+      {isDisplayChatOnSettingMenu && (
+        <div className={styles.root}>
+          <IconButton
+            className={styles.button}
+            aria-label="more"
+            id="long-button"
+            aria-controls={open ? 'long-menu' : undefined}
+            aria-expanded={open ? 'true' : undefined}
+            aria-haspopup="true"
+            onClick={handleClick}
+          >
+            <img src={chrome.runtime.getURL(Logo)} alt={manifest.name} />
+          </IconButton>
 
-      <Menu
-        id="long-menu"
-        MenuListProps={{
-          'aria-labelledby': 'long-button',
-          disablePadding: true
-        }}
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        PaperProps={{
-          style: {
-            maxHeight: ITEM_HEIGHT * 4.5
-          }
-        }}
-      >
-        <MenuList>
-          <MenuHeader manifest={manifest} />
-          <Divider />
-          <MenuReapply handleClose={handleClose} />
-          <MenuOpenPopupPage handleClose={handleClose} />
-          <Divider />
-          <MenuFooter manifest={manifest} />
-        </MenuList>
-      </Menu>
-    </div>
+          <Menu
+            id="long-menu"
+            MenuListProps={{
+              'aria-labelledby': 'long-button',
+              disablePadding: true
+            }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            PaperProps={{
+              style: {
+                maxHeight: ITEM_HEIGHT * 4.5
+              }
+            }}
+          >
+            <MenuList>
+              <MenuHeader manifest={manifest} />
+              <Divider />
+              <MenuReapply handleClose={handleClose} />
+              <MenuOpenPopupPage handleClose={handleClose} />
+              <Divider />
+              <MenuFooter manifest={manifest} />
+            </MenuList>
+          </Menu>
+        </div>
+      )}
+    </>
   )
 }
 
